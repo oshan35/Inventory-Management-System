5876d47ad619d054f39c431bc6e5e04af0671bed
oshan devinda <oshan.devinda35@gmail.com>
2023-08-25 22:01:15 +0530

    components

---
+++ src/components/hedder/hedder.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './hedder.css'
+
+const PageHedder = () => {
+    return(
+        <>
+            <div className='hedder'>
+                <div className='nav-icon'>
+                    <svg className='icon'>
+                        <path d="M26 10H6" stroke="#605DEC" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        <path d="M22 16H6" stroke="#605DEC" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        <path d="M18 22H6" stroke="#605DEC" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
+                </div>
+                <div className='name'>
+                    <span className='company-name'>InventoSync</span>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default PageHedder;
