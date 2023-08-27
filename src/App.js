9618609daa1502a1966ad61c5aac5411562c74cf
oshan devinda <oshan.devinda35@gmail.com>
2023-08-27 11:08:51 +0530

    added hedder page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import NavbarEdited from './components/navbar/nav';
 import PageHedder from './components/hedder/hedder';
+import LoginPage from './Pages/LoginPage/loginPage';
 
 function App() {
   return (
     <div className="App">
-      {/* <NavbarEdited/> */}
-      <PageHedder/>
+      <LoginPage/>
     </div>
   );
 }
